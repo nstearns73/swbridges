6be5bef71a0a8c01f4716e5f4e642c64bff50b3c
nstearns73 <131477463+nstearns73@users.noreply.github.com>
2023-04-22 11:27:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
-from flask import Flask, request, render_template_string
-from caesar_cipher import encrypt_caesar_cipher, main
+from flask import Flask, render_template, request, jsonify
+from caesar_cipher import encrypt_caesar_cipher
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/encrypt', methods=['POST'])
-def cipher_game():
-    message = "Look under the podium!"
-    shift = 3
-    encrypted_message = encrypt_caesar_cipher(message, shift)
-    user_input = ""
-
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        if user_input.lower() == message.lower():
-            return render_template_string("Congratulations! You've successfully decrypted the message!")
-
-    return render_template_string('''
-        <h1>Welcome to the Cipher Game!</h1>
-        <p>Solve the following Caesar cipher to reveal the hidden message:</p>
-        <p>{{ encrypted_message }}</p>
-        <form method="post">
-            <input type="text" name="user_input" placeholder="Enter your solution" required>
-            <button type="submit">Submit</button>
-        </form>
-        {% if user_input %}
-            <p>Incorrect! Try again.</p>
-        {% endif %}
-    ''', encrypted_message=encrypted_message, user_input=user_input)
+def encrypt_route():
+    data = request.get_json()
+    text = data.get('text', '')
+    shift = data.get('shift', 0)
+    encrypted_text = encrypt_caesar_cipher(text, shift)
+    return jsonify({"encrypted_text": encrypted_text})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
