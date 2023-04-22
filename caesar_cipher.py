0a71f5a12ab77460bce0ebb1c3b8a124e283fa27
nstearns73 <131477463+nstearns73@users.noreply.github.com>
2023-04-22 10:27:01 -0700

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,6 +1,25 @@
 def encrypt_caesar_cipher(text, shift):
     encrypted_text = ""
 
+    # caesar_cipher.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def encrypt_caesar_cipher(text, shift):
+    # ... (the rest of the function code)
+
+@app.route('/encrypt', methods=['POST'])
+def encrypt_route():
+    data = request.get_json()
+    text = data.get('text', '')
+    shift = data.get('shift', 0)
+    encrypted_text = encrypt_caesar_cipher(text, shift)
+    return jsonify({"encrypted_text": encrypted_text})
+
+if __name__ == '__main__':
+    app.run()
+
     for char in text:
         if char.isalpha():
             shift_amount = shift % 26
